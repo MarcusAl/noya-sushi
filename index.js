def7e5ed4013aca2cc3ec518206f53187eb7fadc
Marcus Allen <emkay1993@gmail.com>
2021-07-21 17:20:25 +0100

    setup heroku

--- index.js
+++ index.js
@@ -100,6 +100,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8000');
+server.listen(5000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 5000');
 });
